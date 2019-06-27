f34f215f579d96105f1d7acf70dfc2e106a2d95b
tamatamvan <sfirst.tama@gmail.com>
2019-06-27 16:54:32 +0700

    spread other props

--- src/Relazsizes.jsx
+++ src/Relazsizes.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import { string, array, object, oneOfType } from 'prop-types'
 
 const DOMisAvailable = window &&
   window.document &&
   window.document.createElement
 
-const Relazsizes = () => (
-  <h1>Hello World</h1>
-)
+if (DOMisAvailable) {
+  require('lazysizes')
+}
+
+class Relazsizes extends Component {
+  render () {
+    const {
+      alt,
+      className,
+      datasrc,
+      datasrcset,
+      src,
+      ...otherProps
+    } = this.props
+
+    return (
+      <img
+        src={src}
+        data-src={datasrc}
+        data-srcset={datasrcset}
+        className={`lazyload ${className ? className : ''}`}
+        alt={alt}
+        { ...otherProps }
+      />
+    )
+  }
+}
+
+Relazsizes.propTypes = {
+  alt: string,
+  className: string,
+  datasrc: string.isRequired,
+  datasrcset: oneOfType([
+    array,
+    object,
+    string
+  ]),
+  src: string,
+}
 
 export default Relazsizes
