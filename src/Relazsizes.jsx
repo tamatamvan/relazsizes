3285c373cf2732dd21e9bfcdf1aeede333e3a03d
tamatamvan <sfirst.tama@gmail.com>
2019-06-27 20:34:44 +0700

    add support for picture element and srcset

--- src/Relazsizes.jsx
+++ src/Relazsizes.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { string, array, object, oneOfType } from 'prop-types'
+import { string, arrayOf, shape, oneOfType, oneOf } from 'prop-types'
 
 const DOMisAvailable = window &&
   window.document &&
@@ -11,43 +11,98 @@ if (DOMisAvailable) {
 }
 
 class Relazsizes extends Component {
+  handleSrcset = (el, dataSrcset) => {
+    if (el === 'img' && typeof(dataSrcset) === "object") {
+      return dataSrcset.reduce((acc, val, idx, data) => (
+        `${acc}${val.srcset} ${val.variant}${idx+1 < data.length ? ', ' : ''}`
+      ), '')
+    }
+    if (el === 'picture' && typeof(data)) {
+      return dataSrcset.split(/,.| \w+\, | \w+/)
+        .map(srcset => ({
+          srcset
+        }))
+        .filter(Boolean)
+    }
+    return dataSrcset
+  }
+
   render () {
     const {
       alt,
       className,
       dataSrc,
       dataSrcset,
+      el,
       src,
       ...otherProps
     } = this.props
-
+    const srcSet = dataSrcset && this.handleSrcset(dataSrcset)
+    
     return (
-      <img
-        src={src}
-        data-src={dataSrc}
-        data-srcset={dataSrcset}
-        className={`lazyload ${className ? className : ''}`}
-        alt={alt}
-        { ...otherProps }
-      />
+      <>
+        {{
+          picture: (
+            <picture>
+              {
+                srcSet && srcSet.map(val => (
+                  <source data-srcset={val.srcset} media={val.media} type={val.type}/>
+                ))
+              }
+              <img
+                src={src}
+                data-src={dataSrc}
+                className={`lazyload ${className ? className : ''}`}
+                alt={alt}
+                {...otherProps}
+              />
+            </picture>
+          ),
+          img: (
+            <img
+              src={src}
+              data-src={dataSrc}
+              data-srcset={srcSet}
+              className={`lazyload ${className ? className : ''}`}
+              alt={alt}
+              {...otherProps}
+            />
+          ),
+          iframe: (
+            <iframe />
+          )
+        }[el]}
+      </>
     )
   }
 }
+
+const srcSetItem = shape({
+  media: string,
+  srcset: string.isRequired,
+  type: string,
+  variant: string,
+})
 
 Relazsizes.propTypes = {
   alt: string,
   className: string,
   dataSrc: string.isRequired,
   dataSrcset: oneOfType([
-    array,
-    object,
+    arrayOf(srcSetItem),
     string
+  ]),
+  el: oneOf([
+    'img',
+    'picture',
+    'iframe'
   ]),
   src: string,
 }
 
 Relazsizes.defaultProps = {
   src: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+  el: 'img'
 }
 
 export default Relazsizes
